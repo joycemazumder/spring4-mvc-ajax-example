7328ec7ad2dc09bf1229522cf95b8957925913be
joycemazumder123 <joycemazumder@hotmail.com>
2019-07-08 21:36:12 +0530

    updated

--- src/main/java/com/mkyong/config/SpringWebConfig.java
+++ src/main/java/com/mkyong/config/SpringWebConfig.java
@@ -1,5 +1,5 @@
 package com.mkyong.config;
- 
+ //webconfig
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
